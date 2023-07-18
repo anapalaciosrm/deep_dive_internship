0fa8593fb2c935deb81d57a4b831128ac55d0aa5
anapalaciosrm <135923938+anapalaciosrm@users.noreply.github.com>
2023-07-18 12:20:36 -0700

    Update creandoCSV.py
    Use este code para crear el CSV, no es necesario 
    
    Signed-off-by: anapalaciosrm <135923938+anapalaciosrm@users.noreply.github.com>

--- creandoCSV.py
+++ creandoCSV.py
@@ -4,10 +4,10 @@ import pandas as pd
 def create_csv_file(file_path):
 
     data = np.array([
-        ['Nombre', 'Carrera', 'Año', 'deep_dive_response'],
-        ['Abadi Cherem Elias', 'Economía', '2011',''],
-        ['Abad Lopez Carlos Adrian', 'Matemáticas Aplicadas', '2002',''],
-        ['Miguel Bosé ', 'Economía', '1900','']
+        ['Nombre', 'Carrera', 'Año'],
+        ['Abadi Cherem Elias', 'Economía', '2011'],
+        ['Abad Lopez Carlos Adrian', 'Matemáticas Aplicadas', '2002'],
+        ['Miguel Bosé ', 'Economía', '1900']
     ])
 
     df = pd.DataFrame(data=data[1:, :], columns=data[0, :])
